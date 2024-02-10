189fa37f1c68221599c3acdfc47255e33c811666
Ольга Лосева <olgalo@list.ru>
2024-02-10 14:03:41 +0300

    Добавьте файлы проекта.

---
+++ Lab14/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class Dog : Animal
+    {
+        string name;
+        
+        public override string Name { get => name; set => name = value; }
+
+        public Dog(string name)
+             : base(name)
+        {
+           
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+}
